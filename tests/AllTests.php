678aea7dd617cc06c6915979d99010653ba3e878
Benjamin Eberlei <kontakt@beberlei.de>
2009-08-14 18:32:26 +0200

    Add Test for Yadif_Builder

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once dirname(__FILE__)."/../Container.php";
+require_once dirname(__FILE__)."/../Builder.php";
+require_once dirname(__FILE__)."/../Module.php";
 require_once "PHPUnit/Framework.php";
 
 require_once "YadifConfigComponentTest.php";
@@ -8,6 +10,7 @@ require_once "YadifInstantiateObjectGraphTest.php";
 require_once "YadifBindParamsTest.php";
 require_once "YadifEnforceSingletonTest.php";
 require_once "YadifZendApplicationComplianceTest.php";
+require_once "YadifBuilderTest.php";
 
 class AllTests
 {
@@ -19,6 +22,7 @@ class AllTests
         $suite->addTestSuite('YadifBindParamsTest');
         $suite->addTestSuite('YadifEnforceSingletonTest');
         $suite->addTestSuite('YadifZendApplicationComplianceTest');
+        $suite->addTestSuite('YadifBuilderTest');
 
         return $suite;
     }
